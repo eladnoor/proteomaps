42a0bc7cee66fa15af69f82673f5f4cbfe61316e
Dan Davidi <dandavidi@gmail.com>
2014-01-19 12:48:47 +0200

    adding documentation to driking KEGG script

--- src/drink_kegg_orthology.py
+++ src/drink_kegg_orthology.py
@@ -41,12 +41,20 @@ def FromKeggAPI(s):
 csv_reader = csv.reader(urllib.urlopen('http://rest.kegg.jp/list/%s/' % ABBREV),
                         delimiter='\t')
 all_dbentries = []
-for line in csv_reader:
-    dbentry = line[0]
+for row in csv_reader:
+    dbentry = row[0]
     all_dbentries.append(dbentry)
-    if len(all_dbentries) > 2:
-        break
     
+    # this 'break' is only for testing purposes (to reduce overall runtime)
+    # should be removed when actually running the script for real    
+    if len(all_dbentries) == 2:
+        break
+
+# split the list of entires into many sub-lists in order to
+# no overwhelm the KEGG server and request only a handful of
+# entries every time. remember that the API is not designed for
+# exhaustive download of the site, so more than 10 entires at a 
+# time might be too much.
 chunks = [all_dbentries[i:i+KEGG_STEP]
           for i in xrange(0, len(all_dbentries), KEGG_STEP)]
 
@@ -60,11 +68,11 @@ for dbentries in chunks:
 
     entry_dict = FromKeggAPI(s)
     for dbentry, field_map in sorted(entry_dict.iteritems()):
-        brite = field_map[FIELD_NAME]
-        if brite is not None:
+        field_val = field_map[FIELD_NAME]
+        if field_val is not None:
             print dbentry
-            print brite
+            print field_val
             print '-' * 50
         else:
-            sys.stderr.write("WARNING: %s does not contain a BRITE field" %
-                             dbentry)
+            sys.stderr.write("WARNING: %s does not contain a %s field" %
+                             (FIELD_NAME, dbentry))
